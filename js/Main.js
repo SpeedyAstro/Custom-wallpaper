16b4409f3594dea67c6853890bf8f7058fa1cfd6
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-10-01 18:49:19 +0530

    final

---
+++ js/Main.js
@@ -0,0 +1,15 @@
+
+new TypeIt("#myElement", {
+   
+    speed: 70,
+    deleteSpeed: 80,
+    waitUntilVisible: true,
+    loop: true,
+    cursor: true,
+    cursorSpeed: 1000,
+    cursorChar: "|",
+    startDelay: null,
+    nextStringDelay: 1500,
+    loopDelay: 5000,
+    html: true,
+}).go();
